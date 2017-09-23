1fbbcda88247a47cf3ad5e1554a71f5aa0186b3d
AlexandreRouma <alexandre.rouma@gmail.com>
2017-09-23 20:53:42 +0200

    Changed tabs to spaces

--- SIMM.cpp
+++ SIMM.cpp
@@ -2,7 +2,7 @@
  * File:    SIMM.cpp
  * Date:    23/09/2017
  * Author:  Alexandre Rouma (@WhatsTheGeekYT twitter)
- * Version:	1.1.0
+ * Version: 1.1.0
  */
  
 // Important: Keep in mind that CAS, RAS and WE are inverted, so when I write 0, it means enabled !
@@ -16,105 +16,105 @@ SIMMClass SIMM;
 
 void refreshRam() {
   for (int i = 0; i < 4096; i++) {
-    PORTC = PORTC & B11011111;				// Enable CAS
-    PORTC = PORTC & B01111111;				// Enable RAS
-    PORTC = PORTC | B00100000;				// Disable CAS
-    PORTC = PORTC | B10000000;				// Disable RAS
+    PORTC = PORTC & B11011111;              // Enable CAS
+    PORTC = PORTC & B01111111;              // Enable RAS
+    PORTC = PORTC | B00100000;              // Disable CAS
+    PORTC = PORTC | B10000000;              // Disable RAS
   }
 }
 
 void SIMMClass::begin(){
-	cli();									// Disable interrupts while we set up everything
+    cli();                                  // Disable interrupts while we set up everything
 
-	Timer3.initialize(62000);				// Set Timer3 to fire every 62ms
-	Timer3.attachInterrupt(&refreshRam);	// Attach Timer3 interrupt to the RAM refresh routine
+    Timer3.initialize(62000);               // Set Timer3 to fire every 62ms
+    Timer3.attachInterrupt(&refreshRam);    // Attach Timer3 interrupt to the RAM refresh routine
 
-	DDRL = 0b00000000;						// Set PORTL (the data bus) to input
-	DDRA = 0b11111111;						// Set LB Address to output
-	DDRC = 0b11111111;						// Set HB Address and controls to output
-	PORTA = 0b00000000;						// Clear PORTA
-	PORTC = 0b11100000;						// Clear PORTC and set RAS, CAS and WE to HIGH (remember these three are inverted)
+    DDRL = 0b00000000;                      // Set PORTL (the data bus) to input
+    DDRA = 0b11111111;                      // Set LB Address to output
+    DDRC = 0b11111111;                      // Set HB Address and controls to output
+    PORTA = 0b00000000;                     // Clear PORTA
+    PORTC = 0b11100000;                     // Clear PORTC and set RAS, CAS and WE to HIGH (remember these three are inverted)
 
-	_delay_us(200);							// Wait 200 microseconds to be sure the RAM is ready
-	for (uint8_t t = 0; t < 8; t++) {		// Initialize the RAM
-		refreshRam();
-	}
+    _delay_us(200);                         // Wait 200 microseconds to be sure the RAM is ready
+    for (uint8_t t = 0; t < 8; t++) {       // Initialize the RAM
+        refreshRam();
+    }
 
-	sei();									// Re-enable interrupts
+    sei();                                  // Re-enable interrupts
 }
 
 void SIMMClass::stop(){
-	Timer3.detachInterrupt();
+    Timer3.detachInterrupt();
 }
 
 byte SIMMClass::read(uint16_t row, uint16_t column){
-	byte data;								// This is the buffer used for the data from the RAM
+    byte data;                              // This is the buffer used for the data from the RAM
 
-	cli();
+    cli();
 
-	PORTA = row;							// Write lower row address byte to PORTA
-	PORTC = PORTC | (row >> 8);				// Write lower row address byte to PORTA
-	PORTC = PORTC & B01111111;				// Enable RAS
+    PORTA = row;                            // Write lower row address byte to PORTA
+    PORTC = PORTC | (row >> 8);             // Write lower row address byte to PORTA
+    PORTC = PORTC & B01111111;              // Enable RAS
 
-	PORTA = column;							// Write lower column address byte to PORTA
-	PORTC = PORTC | (column >> 8);			// Write high column address byte to PORTA
-	PORTC = PORTC & B11011111;				// Enable CAS
+    PORTA = column;                         // Write lower column address byte to PORTA
+    PORTC = PORTC | (column >> 8);          // Write high column address byte to PORTA
+    PORTC = PORTC & B11011111;              // Enable CAS
 
-	PORTC = PORTC | B10000000;				// Disable RAS
-	PORTC = PORTC | B00100000;				// Disable CAS
+    PORTC = PORTC | B10000000;              // Disable RAS
+    PORTC = PORTC | B00100000;              // Disable CAS
 
-	data = PINL;							// Read data from PORTL
-	sei();
+    data = PINL;                            // Read data from PORTL
+    sei();
 
-	return data;
+    return data;
 }
 
 void SIMMClass::write(uint16_t row, uint16_t column, byte data){
-	cli();									// Disable interrupts while we write
+    cli();                                  // Disable interrupts while we write
 
-	DDRL = B11111111;						// Set PORTL as output;
+    DDRL = B11111111;                       // Set PORTL as output;
 
-	PORTA = row;							// Write lower row address byte to PORTA
-	PORTC = PORTC | (row >> 8);				// Write high row address byte to PORTA
-	PORTC = PORTC & B01111111;				// Enable RAS
+    PORTA = row;                            // Write lower row address byte to PORTA
+    PORTC = PORTC | (row >> 8);             // Write high row address byte to PORTA
+    PORTC = PORTC & B01111111;              // Enable RAS
 
-	PORTL = data;							// Write our value to RAM
+    PORTL = data;                           // Write our value to RAM
 
-	PORTC = PORTC & B10111111;				// Enable WE
+    PORTC = PORTC & B10111111;              // Enable WE
 
-	PORTA = column;							// Write lower column address byte to PORTA
-	PORTC = PORTC | (column >> 8);			// Write high column address byte to PORTC
+    PORTA = column;                         // Write lower column address byte to PORTA
+    PORTC = PORTC | (column >> 8);          // Write high column address byte to PORTC
 
-	PORTC = PORTC & B11011111;				// Strobe CAS. One clock cycle is enough for the ram to register !
-	PORTC = PORTC | B00100000;
+    PORTC = PORTC & B11011111;              // Strobe CAS. One clock cycle is enough for the ram to register !
+    PORTC = PORTC | B00100000;
 
-	PORTC = PORTC | B01000000;				// Disable WE
-	PORTC = PORTC | B10000000;				// Disable RAS
+    PORTC = PORTC | B01000000;              // Disable WE
+    PORTC = PORTC | B10000000;              // Disable RAS
 
-	DDRL = B00000000;						// Set PORTL back to input
+    DDRL = B00000000;                       // Set PORTL back to input
 
-	sei();									// Re-enable interrupts
+    sei();                                  // Re-enable interrupts
 }
 
 void SIMMClass::readBlock(uint16_t row, uint16_t column, uint16_t length, byte array[]){
-	for (uint16_t i = 0; i < length; i++){	// Cycle through addresses and fill up the array
-		if (column >= 4096){
-			column = 0;						// If the column is greater or equal to 4096, increment
-			row++;							// the rows and set column back to 0
-		}
-		array[i] = read(row, column);		// Write the data to the array
-		column++;							// Increment the column
-	}
+    for (uint16_t i = 0; i < length; i++){  // Cycle through addresses and fill up the array
+        if (column >= 4096){
+            column = 0;                     // If the column is greater or equal to 4096, increment
+            row++;                          // the rows and set column back to 0
+        }
+        array[i] = read(row, column);       // Write the data to the array
+        column++;                           // Increment the column
+    }
 }
 
 void SIMMClass::writeBlock(uint16_t row, uint16_t column, byte array[]){
-	uint16_t length = sizeof(array);		// Get the size of the array
-	for (uint16_t i = 0; i < length; i++){	// Cycle through the array write the values to RAM
-		if (column >= 4096){
-			column = 0;						// If the column is greater or equal to 4096, increment
-			row++;							// the rows and set column back to 0
-		}
-		write(row, column, array[i]);		// Write the data to RAM
-		column++;							// Increment the column
-	}
+    uint16_t length = sizeof(array);        // Get the size of the array
+    for (uint16_t i = 0; i < length; i++){  // Cycle through the array write the values to RAM
+        if (column >= 4096){
+            column = 0;                     // If the column is greater or equal to 4096, increment
+            row++;                          // the rows and set column back to 0
+        }
+        write(row, column, array[i]);       // Write the data to RAM
+        column++;                           // Increment the column
+    }
 }
--- SIMM.h
+++ SIMM.h
@@ -2,17 +2,17 @@
  * File:    SIMM.h
  * Date:    23/09/2017
  * Author:  Alexandre Rouma (@WhatsTheGeekYT twitter)
- * Version:	1.1.0
+ * Version: 1.1.0
  */
 
 class SIMMClass {
-	public:
-		void begin();
-		void stop();
-		byte read(uint16_t row, uint16_t column);
-		void write(uint16_t row, uint16_t column, byte data);
-		void readBlock(uint16_t row, uint16_t column, uint16_t length, byte array[]);
-		void writeBlock(uint16_t row, uint16_t column, byte array[]);
+    public:
+        void begin();
+        void stop();
+        byte read(uint16_t row, uint16_t column);
+        void write(uint16_t row, uint16_t column, byte data);
+        void readBlock(uint16_t row, uint16_t column, uint16_t length, byte array[]);
+        void writeBlock(uint16_t row, uint16_t column, byte array[]);
 };
 
 extern SIMMClass SIMM;
